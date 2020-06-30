5a12ae2260c67fef357b764047cf02fc6614a8c7
Reeta Shrestha <retdream@gmail.com>
2020-06-30 23:18:53 +1000

    Log rgb values on mousemove

--- src/Huw.js
+++ src/Huw.js
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export const Huw = () => {
-  const size = 3;
-  const width = 128;
   const ref = useRef();
+  let [color, setColor] = useState("");
+  const size = 25;
+  const width = 32;
   const getRGBList = () => {
     const rgbCollection = [];
     for (let r = 0; r < 32; r++) {
@@ -18,7 +19,20 @@ export const Huw = () => {
 
   useEffect(() => {
     const rgbList = getRGBList();
-    let ctx = ref.current.getContext("2d");
+    let canvas = ref.current;
+    let ctx = canvas.getContext("2d");
+
+    canvas.addEventListener("mousemove", (e) => {
+      let i =
+        Math.floor(e.offsetY / size) * width + Math.floor(e.offsetX / size);
+      // There's a race condition where mousemove is called with coordinates outside the canvas
+      if (i < 0 || i > 32767) {
+        return;
+      }
+      const rgbValue = `rgb(${rgbList[i][0]},${rgbList[i][1]},${rgbList[i][2]})`;
+      setColor(rgbValue);
+    });
+
     for (let y = 0; y < 32768 / width; y++) {
       for (let x = 0; x < width; x++) {
         let i = y * width + x;
@@ -28,10 +42,15 @@ export const Huw = () => {
     }
   });
   return (
-    <canvas
-      ref={ref}
-      width={width * size}
-      height={(size * 32768) / width}
-    ></canvas>
+    <>
+      {color && (
+        <h2 style={{ position: "fixed", color: `${color}` }}>Color: {color}</h2>
+      )}
+      <canvas
+        ref={ref}
+        width={width * size}
+        height={(size * 32768) / width}
+      ></canvas>
+    </>
   );
 };
